2413135207cbf3193abc3faccfeafc69202f931b
Alagappan <43281916+alagappan28@users.noreply.github.com>
2019-04-12 17:56:22 -0400

    inital step

--- ir-system.py
+++ ir-system.py
@@ -1,13 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr 10 23:06:59 2019
-
-@author: Pranav Krishna
-"""
 import re
-fname = r"D:\bin\AIT-690\Assignments\IR\cran.all.1400"
-#docfile
+from nltk.tokenize import word_tokenize
 
+#fname = r"D:\bin\AIT-690\Assignments\IR\cran.all.1400"
+content = r"C:\Users\alaga\Desktop\sem 2\AIT690\IR1\cran.all.1400"
+#docfile
+content=open(content)
+content=content.read()
+content = content.replace("\n", "")
+content=content.replace(".I"," .I")
+content=content.replace(".T"," .T ")
+content=content.replace(".W"," .W ")
+#content=content.replace(".W",".W ")
+
+
+#temp=word_tokenize(content)
+
+temp=content.split()
+
+id=[]
+title=[]
+body=[]
+flag=-1
+flag1=-1
+cnt=0
+
+for i in range(0,len(temp)):
+    if '.I'in temp[i]:
+        body.append("*")
+        flag=0
+    elif(flag==0):
+        id.append(temp[i])
+        flag=1
+    elif(flag==1):
+        if('.T' in temp[i]):
+            title.append(temp[i])
+            flag=2
+        elif(flag==2 and '.A' not in temp[i]):
+            title.append(temp[i])
+
+    elif temp[i]=='.W':
+        flag=3
+    elif(flag==3):
+        body.append(temp[i])
+
+
+
+title_split=[]
+for i in title:
+    if i=='*':
+        temp=" ".join(temp)
+        title_split.append(temp)
+        temp=[]
+
+    else:
+        temp.append(i)
+
+'''
 docs={}
 prevPointer=None
 topic=None
@@ -26,5 +74,9 @@ with open(fname) as f:
             text=line
 
         prevPointer=str(line)
-        
-    
+'''
+cnt=0
+for i in range(0,len(id)):
+    if i+1==int(id[i]):
+        cnt+=1
+print(cnt)
